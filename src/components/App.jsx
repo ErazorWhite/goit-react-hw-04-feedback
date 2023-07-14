ccc9e3317d97fd6b3f4d4e74bf5c23c22fae00ec
ErazorWhite <41112771+ErazorWhite@users.noreply.github.com>
2023-07-14 21:04:02 +0300

    refactored: class -> hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,96 @@
+import { useState } from 'react';
 import { Component } from 'react';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Notification from './Notification/Notification';
 
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  handleClickFeedBack = feedbackType => {
-    this.setState(prevState => ({
-      [feedbackType]: prevState[feedbackType] + 1,
+  const handleClickFeedback = feedbackType => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
     }));
   };
 
-  countTotalFeedback = () => {
-    const stateValues = Object.values(this.state);
-    return stateValues.reduce((acc, curr) => acc + curr, 0);
+  const countTotalFeedback = () => {
+    const feedbackValues = Object.values(feedback);
+    return feedbackValues.reduce((acc, curr) => acc + curr, 0);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
-    return Math.round((good * 100) / total) || 0;
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = feedback;
+    const total = countTotalFeedback();
+    return Math.round((good * 100) / total) ?? 0;
   };
 
-  render() {
-    const total = this.countTotalFeedback();
-    return (
-      <>
-        <FeedbackOptions
-          options={Object.keys(this.state)}
-          onLeaveFeedback={this.handleClickFeedBack}
+  const total = countTotalFeedback();
+  return (
+    <>
+      <FeedbackOptions
+        options={Object.keys(feedback)}
+        onLeaveFeedback={handleClickFeedback}
+      />
+      {total ? (
+        <Statistics
+          options={feedback}
+          total={total}
+          positivePercentage={countPositiveFeedbackPercentage()}
         />
-        {total ? (
-          <Statistics
-            options={this.state}
-            total={total}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </>
+  );
+};
+
+// export class OldApp extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   handleClickFeedBack = feedbackType => {
+//     this.setState(prevState => ({
+//       [feedbackType]: prevState[feedbackType] + 1,
+//     }));
+//   };
+
+//   countTotalFeedback = () => {
+//     const stateValues = Object.values(this.state);
+//     return stateValues.reduce((acc, curr) => acc + curr, 0);
+//   };
+
+//   countPositiveFeedbackPercentage = () => {
+//     const { good } = this.state;
+//     const total = this.countTotalFeedback();
+//     return Math.round((good * 100) / total) || 0;
+//   };
+
+//   render() {
+//     const total = this.countTotalFeedback();
+//     return (
+//       <>
+//         <FeedbackOptions
+//           options={Object.keys(this.state)}
+//           onLeaveFeedback={this.handleClickFeedBack}
+//         />
+//         {total ? (
+//           <Statistics
+//             options={this.state}
+//             total={total}
+//             positivePercentage={this.countPositiveFeedbackPercentage()}
+//           />
+//         ) : (
+//           <Notification message="There is no feedback" />
+//         )}
+//       </>
+//     );
+//   }
+// }
